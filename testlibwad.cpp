1d1f65b21319ec66b53553903d89f599b8c0271b
NathanMcTear <nmctear@ufl.edu>
2025-04-16 22:21:30 +0000

    Added further test functionality

--- testlibwad.cpp
+++ testlibwad.cpp
@@ -3,8 +3,9 @@
 #include "libWad/Wad.h"
 
 using namespace std;
+int testContentNDirs();
 
-void test(Wad* wad, const string& path, bool expectContent, bool expectDirectory) {
+void unitTestContentNDirs(Wad* wad, const string& path, bool expectContent, bool expectDirectory) {
     bool actualContent = wad->isContent(path);
     bool actualDirectory = wad->isDirectory(path);
 
@@ -13,6 +14,14 @@ void test(Wad* wad, const string& path, bool expectContent, bool expectDirectory
 }
 
 int main() {
+    int score = 0;
+    int tests = 0;
+    score = testContentNDirs();
+    tests++;
+    cout << "Overall Test Score: " << score << "/" << tests << endl;
+}
+
+int testContentNDirs() {
     cout << "Testing isContent & isDirectory:" << endl;
     Wad* wad = Wad::loadWad("libWad/P3Files/sample1.wad");
     if (!wad) {
@@ -21,40 +30,40 @@ int main() {
     }
 
     // Root
-    test(wad, "/", false, true);
+    unitTestContentNDirs(wad, "/", false, true);
 
     // Namespace directories
-    test(wad, "/E1M0", false, true);
-    test(wad, "/E1M0/01.txt", true, false);
-    test(wad, "/E1M0/02.txt", true, false);
-    test(wad, "/E1M0/03.txt", true, false);
-    test(wad, "/E1M0/04.txt", true, false);
-    test(wad, "/E1M0/05.txt", true, false);
-    test(wad, "/E1M0/06.txt", true, false);
-    test(wad, "/E1M0/07.txt", true, false);
-    test(wad, "/E1M0/08.txt", true, false);
-    test(wad, "/E1M0/09.txt", true, false);
-    test(wad, "/E1M0/10.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0", false, true);
+    unitTestContentNDirs(wad, "/E1M0/01.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/02.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/03.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/04.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/05.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/06.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/07.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/08.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/09.txt", true, false);
+    unitTestContentNDirs(wad, "/E1M0/10.txt", true, false);
 
     // Name Space dir
-    test(wad, "/Gl", false, true);
+    unitTestContentNDirs(wad, "/Gl", false, true);
 
     // File inside Gl
-    test(wad, "/Gl/ad", false, true);
+    unitTestContentNDirs(wad, "/Gl/ad", false, true);
 
     // File inside ad
-    test(wad, "/Gl/ad/os", false, true);
+    unitTestContentNDirs(wad, "/Gl/ad/os", false, true);
 
     // File inside os
-    test(wad, "/Gl/ad/os/cake.jpg", true, false);
+    unitTestContentNDirs(wad, "/Gl/ad/os/cake.jpg", true, false);
 
     // root file
-    test(wad, "/mp.txt", true, false);
+    unitTestContentNDirs(wad, "/mp.txt", true, false);
 
     // Made-up path
-    test(wad, "/fake/path/ghost", false, false);
+    unitTestContentNDirs(wad, "/fake/path/ghost", false, false);
 
     delete wad;
-    cout << "All tests passed!" << endl;
-    return 0;
+    cout << "Passed!" << endl;
+    return 1;
 }
